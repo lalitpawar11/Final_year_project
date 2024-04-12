62c272c013bbb0e7052ece54afba9fe9b0f67302
lalitpawar11 <159535510+lalitpawar11@users.noreply.github.com>
2024-04-12 15:17:39 +0530

    Update environment.ts

--- Client/src/environments/environment.ts
+++ Client/src/environments/environment.ts
@@ -2,9 +2,9 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false
-};
+// export const environment = {
+//   production: false
+// };
 
 export const infuraAPI = 'https://ipfs.infura.io/ipfs/'
 export const localAPI = 'http://127.0.0.1:8080/ipfs/'
